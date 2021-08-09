f0edd68339d4625cfbde9bfd3ca92da5225ac843
Muhammad ASIF <Muhammad.Asif@veripark.com>
2021-08-09 19:49:35 +0500

    removed .net core dependency and ILMerge

--- SolutionComparer/Utilities/NugetPackageManager.cs
+++ SolutionComparer/Utilities/NugetPackageManager.cs
@@ -1,55 +1,128 @@
-﻿using NuGet;
+﻿using NuGet.Common;
+using NuGet.Packaging;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CRMSolutionComparer.Utilities
 {
     class NugetPackageManager
     {
+        string packageID = "Microsoft.CrmSdk.CoreTools";
+        NuGetVersion version = new NuGetVersion("9.1.0.79");
         public void DownloadPackage()
         {
-            //ID of the package to be looked up 
-            string packageID = "Microsoft.CrmSdk.CoreTools";
 
+            //string packageID = "Microsoft.CrmSdk.CoreTools";
+            //IPackageRepository repo = PackageRepositoryFactory.Default.CreateRepository("https://packages.nuget.org/v1/FeedService.svc/");
 
-            //Connect to the official package repository 
-            IPackageRepository repo = PackageRepositoryFactory.Default.CreateRepository("https://packages.nuget.org/v1/FeedService.svc/");
+            //string filePath = System.IO.Path.GetTempPath();
+            //filePath += "\\" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + "\\";
 
-            /*
-            //Get the list of all NuGet packages with ID 'EntityFramework' 
-            List<IPackage> packages = repo.FindPackagesById(packageID).ToList();
+            //string solutionPackagerFileName = Path.Combine($@"{filePath}Microsoft.CrmSdk.CoreTools.9.1.0.79\content\bin\coretools", "SolutionPackager.exe");
 
-            //Filter the list of packages that are not Release (Stable) versions 
-            packages = packages.Where(item => (item.IsReleaseVersion() == false)).ToList();
-            //Iterate through the list and print the full name of the pre-release packages to console 
-            foreach (IPackage p in packages)
-            {
-                Console.WriteLine(p.GetFullName());
-            }
-            */
+            //if (!File.Exists(solutionPackagerFileName))
+            //{
+            //    Console.WriteLine($"Downloading solution packager on path {filePath}");
+            //    PackageManager packageManager = new PackageManager(repo, filePath);
+            //    //Download and unzip the package 
+            //    packageManager.InstallPackage(packageID, SemanticVersion.Parse("9.1.0.79"));
+            //    Console.WriteLine($"Solution packager downloaded on path {filePath}");
+            //}
+            //else {
+            //    Console.WriteLine($"Solution packager already exist on path {solutionPackagerFileName}");
+            //}
 
-            //Initialize the package manager string path = <PATH_TO_WHERE_THE_PACKAGES_SHOULD_BE_INSTALLED>
-            //string filePath = new FileInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).Directory.ToString();
             string filePath = System.IO.Path.GetTempPath();
             filePath += "\\" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + "\\";
 
-            string solutionPackagerFileName = Path.Combine($@"{filePath}Microsoft.CrmSdk.CoreTools.9.1.0.79\content\bin\coretools", "SolutionPackager.exe");
+            string solutionPackagerFileName = Path.Combine($@"{filePath}{packageID}.{version.Version}\content\bin\coretools", "SolutionPackager.exe");
 
             if (!File.Exists(solutionPackagerFileName))
             {
                 Console.WriteLine($"Downloading solution packager on path {filePath}");
-                PackageManager packageManager = new PackageManager(repo, filePath);
-                //Download and unzip the package 
-                packageManager.InstallPackage(packageID, SemanticVersion.Parse("9.1.0.79"));
-                Console.WriteLine($"Solution packager downloaded on path {filePath}");
+
+                var task = DownloadPackageAsync();
+                task.Wait();
+
             }
-            else {
+            else
+            {
                 Console.WriteLine($"Solution packager already exist on path {solutionPackagerFileName}");
             }
+
+        }
+        private async Task DownloadPackageAsync()
+        {
+            FindPackageByIdResource findPackageById;
+            PackageSearchResource packageSearch;
+            SourceCacheContext cache = new SourceCacheContext();
+            CancellationToken cancellationToken = CancellationToken.None;
+            ILogger logger = NullLogger.Instance;
+
+            SourceRepository repository = Repository.Factory.GetCoreV3("https://api.nuget.org/v3/index.json");
+            packageSearch = repository.GetResource<PackageSearchResource>();
+            findPackageById = repository.GetResource<FindPackageByIdResource>();
+
+
+
+            using (MemoryStream packageStream = new MemoryStream())
+            {
+                if (!await findPackageById.CopyNupkgToStreamAsync(
+                     packageID,
+                     version,
+                     packageStream,
+                     cache,
+                     logger,
+                     cancellationToken))
+                {
+                    throw new Exception($"The Nuget package for tool plugin.NugetId ({version}) has not been found");
+                }
+
+                string filePath = System.IO.Path.GetTempPath();
+                filePath += "\\" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + "\\";
+
+                var packageFolder = Path.Combine(filePath, $"{packageID}.{version.Version}");
+
+                using (PackageArchiveReader packageReader = new PackageArchiveReader(packageStream))
+                {
+                    foreach (var packageFile in await packageReader.GetFilesAsync(cancellationToken))
+                    {
+
+                        if (!Directory.Exists(packageFolder))
+                        {
+                            Directory.CreateDirectory(packageFolder);
+                        }
+
+                        var relativeFilePath = packageFile;
+                        var filePath_ = Path.Combine(packageFolder, relativeFilePath);
+                        var fi = new FileInfo(filePath_);
+
+                        if (!Directory.Exists(fi.Directory.FullName))
+                        {
+                            Directory.CreateDirectory(fi.Directory.FullName);
+                        }
+
+                        using (var fileStream = File.OpenWrite(filePath_))
+                        using (var stream = await packageReader.GetStreamAsync(packageFile, cancellationToken))
+                        {
+                            await stream.CopyToAsync(fileStream);
+                        }
+
+
+                    }
+                }
+                Console.WriteLine($"Solution packager downloaded.");
+            }
+
+           
         }
     }
 }
